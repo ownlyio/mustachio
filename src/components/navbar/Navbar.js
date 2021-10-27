bd3858c2b256375bce9417b0173589f81700af08
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-10-27 17:06:04 +0800

    Add Collectibles in Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -97,6 +97,7 @@ function Navbar(props) {
 					<div className="d-none d-md-block">
                         <ul class="nav justify-content-center mb-0">
                             <li><HashLink smooth to="#tales" scroll={el => scrollWithOffset(el)} title="NFT Tales" className="text-white nav-link font-andes">NFT TALES</HashLink></li>
+                            <li><HashLink smooth to="#collectibles" scroll={el => scrollWithOffset(el)} title="Collectibles" className="text-white nav-link font-andes">COLLECTIBLES</HashLink></li>
                             <li><HashLink smooth to="#roadmap" scroll={el => scrollWithOffset(el)} title="Roadmap" className="text-white nav-link font-andes">ROADMAP</HashLink></li>
                             {/* <li><a href="https://ownly.io/marketplace/?collection=the-mustachios" title="Gallery" className="text-white nav-link font-andes">GALLERY</a></li> */}
                             <li><HashLink smooth to="#team" scroll={el => scrollWithOffset(el)} title="Team" className="text-white nav-link font-andes">TEAM</HashLink></li>
@@ -115,6 +116,7 @@ function Navbar(props) {
                         onStateChange={(state) => handleStateChange(state)}
                     >
                         <HashLink smooth to="#tales" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} title="NFT Tales" className="text-white d-block py-3 w-100 text-decoration-none font-andes">NFT TALES</HashLink>
+                        <HashLink smooth to="#collectibles" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} title="Collectibles" className="text-white d-block py-3 w-100 text-decoration-none font-andes">COLLECTIBLES</HashLink>
                         <HashLink smooth to="#roadmap" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} title="Roadmap" className="text-white d-block py-3 w-100 text-decoration-none font-andes">ROADMAP</HashLink>
                         {/* <a href="https://ownly.io/marketplace/?collection=the-mustachios" onClick={() => closeMenu()} title="Gallery" className="text-white d-block py-3 w-100 text-decoration-none font-andes">GALLERY</a> */}
                         <HashLink smooth to="#team" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} title="Team" className="text-white d-block py-3 w-100 text-decoration-none font-andes">TEAM</HashLink>
