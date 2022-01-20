1ee97f9e26b8ded25eb27cb685426bb05c6e427d
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-01-20 14:55:22 +0800

    Remove Video

--- src/components/mq_info/MQInfo.js
+++ src/components/mq_info/MQInfo.js
@@ -50,9 +50,9 @@ function MQInfo() {
             <div className="container">
                 <div className="row mb-3 align-items-center">
                     <div className="col-lg-5 col-12 mb-3">
-                        <video autoplay loop controls controlsList="nodownload" class="w-100" poster={mq_poster} style={{"borderRadius": "10px", "border": "6px solid #ffffff"}}>
+                        {/* <video autoplay loop controls controlsList="nodownload" class="w-100" poster={mq_poster} style={{"borderRadius": "10px", "border": "6px solid #ffffff"}}>
                             <source src={mq_video} type="video/mp4" />
-                        </video>
+                        </video> */}
                         <div className="d-flex justify-content-between mt-3 mb-4 mb-lg-0">
                             <div onClick={() => showPictureHandler(fp)} title="Click to enlarge" className="cursor-pointer" style={{"borderRadius": "10px", "border": "6px solid #ffffff", "marginRight": "10px"}}>
                                 <img src={fp} className="w-100" alt="FP" />
