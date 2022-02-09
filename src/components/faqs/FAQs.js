e0589f6145bff4d95dff5607b00d9f57efa42717
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-02-10 00:12:42 +0800

    Update FAQs' copy

--- src/components/faqs/FAQs.js
+++ src/components/faqs/FAQs.js
@@ -15,8 +15,8 @@ function FAQs(props) {
                             </h2>
                             <div id="flush-collapse-1" className="accordion-collapse collapse" data-bs-parent="#app-faqs-accordion">
                                 <div className="accordion-body">
-                                    <p className="text-lg font-andes">The Mustachios are the first-ever NFT Tales to be told in the NFT industry. Intricately hand-drawn by the talented <b>Boii Mustache</b>, it took the artist years in the making to actualize the idea of being whatever you want to be, in a land of mustached beings.</p>
-                                    <p className="text-lg font-andes">There will be a total of <b>999 Mustachios only</b>, and for each <b>generation of Mustachios</b> comes a price increase. So don’t forget to Mint your Mustachio today!</p>
+                                    <p className="text-lg font-andes">The Mustachios are the first-ever NFT Tales to be told in the NFT industry. Intricately hand-drawn by the talented Boii Mustache, it took the artist years in the making to actualize the idea of being whatever you want to be, in a land of mustached beings. Now, the concept is solidified through our play-to-earn game, Mustachio Quest, where our Mustachio holders can fulfill quests in an open world NFT game as their 3D Mustachio avatar.</p>
+                                    <p className="text-lg font-andes">There will be a total of 300 Mustachios that come with 2D Mustachio NFTs from our Genesis set and an additional 10,000 3D generative Mustachios known as 3D Playstachios.</p>
                                 </div>
                             </div>
                         </div>
@@ -32,8 +32,8 @@ function FAQs(props) {
                                         <li className="text-lg font-andes">Visit <a href="/">mustachioverse.com</a>.</li>
                                         <li className="text-lg font-andes">Click MINT YOUR MUSTACHIO at the upper right corner.</li>
                                         <li className="text-lg font-andes">Connect your MetaMask wallet (Ethereum Network).</li>
-                                        <li className="text-lg font-andes">Mint your Mustachio for only 0.4 ETH + gas fee.</li>
-                                        <li className="text-lg font-andes">Check your OpenSea account, and there you go! You now OWN a Mustachio.</li>
+                                        <li className="text-lg font-andes">Mint your Mustachio for 0.4 ETH + gas fee.</li>
+                                        <li className="text-lg font-andes">Check your OpenSea or  <a className="text-lg font-andes" href="https://ownly.market/" target="_blank" rel="noreferrer">Ownly Market</a> account, and there you go! You now OWN a Mustachio.</li>
                                     </ol>
                                     <a className="text-lg font-andes" href="https://ownly.io/htmym" target="_blank" rel="noreferrer">Click here for a more detailed instruction.</a>
                                 </div>
@@ -77,7 +77,7 @@ function FAQs(props) {
                                     <p className="text-lg font-andes">Facebook: <a href="https://web.facebook.com/mustachioverse" target="_blank" rel="noreferrer">https://web.facebook.com/mustachioverse</a></p>
                                     <p className="text-lg font-andes">Instagram: <a href="https://www.instagram.com/mustachioverse" target="_blank" rel="noreferrer">https://www.instagram.com/mustachioverse</a></p>
                                     <p className="text-lg font-andes">Twitter: <a href="https://twitter.com/mustachioverse" target="_blank" rel="noreferrer">https://twitter.com/mustachioverse</a></p>
-                                    <p className="text-lg font-andes">Discord: <a href="https://discord.gg/rfypRx4cjh" target="_blank" rel="noreferrer">https://discord.gg/rfypRx4cjh</a></p>
+                                    <p className="text-lg font-andes">Discord: <a href="https://mustachioverse.com/discord" target="_blank" rel="noreferrer">https://mustachioverse.com/discord</a></p>
                                 </div>
                             </div>
                         </div>
@@ -90,10 +90,8 @@ function FAQs(props) {
                             <div id="flush-collapse-6" className="accordion-collapse collapse" data-bs-parent="#app-faqs-accordion">
                                 <div className="accordion-body">
                                     <p className="text-lg font-andes">There are so many possibilities on how you can make use of your Mustachio!</p>
-                                    <p className="text-lg font-andes"><b>The Sages Rant</b> is our official NFT Tales with 10 chapters per book that reveal the stories behind the Mustachios on their quest to find the 9 valuable artifacts that are collectively called the <b>Grooming Kit.</b></p>
-                                    <p className="text-lg font-andes">You can get these artifacts later with our added feature called the <b>Touch Up</b>. We will also give you the joy of changing the background of your Mustachio avatar through our <b>Unlockables</b>.</p>
-                                    <p className="text-lg font-andes">We also have plans on hosting exclusive <b>Mustachios-only events</b> where Mustachios will serve as the ticket voucher for access.</p>
-                                    <p className="text-lg font-andes"><b>3D Mustachio Toys</b> are also on the roster for 2022. Stay tuned!</p>
+                                    <p className="text-lg font-andes">For the Mustachio holders of our genesis set, they get a playable 3D avatar version of their 2D Mustachio. They also get a quarterly OWN token airdrop until our marketing and community airdrops wallet runs out.</p>
+                                    <p className="text-lg font-andes">Aside from partaking in our play-to-earn NFT game, Mustachio Quest, you can also own 9 single-edition artifacts at <a href="https://ownly.market/?collection=the-sages-rant-collectibles" target="_blank" rel="noreferrer">The Sages Rant Collectibles</a>. The Sages Rant is our official NFT Tales with 10 chapters that reveal the stories behind the genesis set of Mustachios on their quest to find the 9 valuable artifacts that are collectively called the Grooming Kit.</p>
                                 </div>
                             </div>
                         </div>
